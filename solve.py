84ae6fb9d66200e79a3f5309fd11791bf9704191
Tengfei Tsui <tioover@gmail.com>
2014-07-08 21:07:47 +0800

    Create solve.py

---
+++ solve.py
@@ -0,0 +1,109 @@
+from heapq import heappop, heappush
+
+
+class Node:
+    def __init__(self, parent, matrix, center, step, shape=None, depth=None):
+        self.shape = parent.shape if parent else shape
+        self.parent = parent
+        self.matrix = matrix
+        self.center = center
+        self.step = step
+        self.depth = parent.depth + 1 if parent else depth
+        self.matrix_distance = self.valuation()
+        self.value = sum(self.matrix_distance)
+
+    def valuation(self):
+        m = self.matrix
+        x, y = self.shape
+        return tuple(
+            (abs(int(i/y) - int(m[i]/y)) + abs((i % y) - (m[i] % y)) for i in range(len(m))))
+
+    def __lt__(self, other):
+        return True
+
+    def __repr__(self):
+        return repr(self.matrix)
+
+
+class Heap:
+    def __init__(self):
+        self.heap = []
+
+    def push(self, node):
+        heappush(self.heap, (node.value, node))
+
+    def pop(self):
+        return heappop(self.heap)[1]
+
+
+def node_generator(node: Node):
+    center = node.center
+    line, row = node.shape
+    matrix = list(node.matrix)
+
+    shift_list = []
+    up = center - row
+    down = center + row
+    left = center - 1
+    right = center + 1
+
+    if right % row != 0:
+        shift_list.append((right, 'r'))
+    if center % row != 0:
+        shift_list.append((left, 'l'))
+    if up >= 0:
+        shift_list.append((up, 'u'))
+    if down < len(matrix):
+        shift_list.append((down, 'd'))
+
+    for shift, name in shift_list:
+        m = matrix[:]
+        m[shift], m[center] = m[center], m[shift]
+        yield Node(node, tuple(m), shift, (name, node.step))
+
+
+def get_center(matrix):
+    return matrix.index(0)
+
+
+def solve(matrix, shape: tuple):
+    center = get_center(matrix)
+    open = Heap()
+    close = {}
+    root = Node(None, matrix, center, (), shape, 0)
+    open.push(root)
+    min_node = root
+    max_loop = 0x50000
+    i = 0
+
+    while i < max_loop:
+        i += 1
+        try:
+            node = open.pop()
+        except IndexError:
+            break
+        if node.value < min_node.value:
+            min_node = node
+        if node.matrix in close:
+            if node.depth > close[node.matrix].depth:
+                close[node.matrix] = node
+            continue
+        elif node.value == 0:
+            return node
+        else:
+            close[node.matrix] = node
+        for new_node in node_generator(node):
+            open.push(new_node)
+    return min_node  # fail
+
+
+def main():
+    import sys
+    shape = tuple(map(int, sys.argv[1:3]))
+    x, y = shape
+    matrix = tuple(map(int, sys.argv[3:3+x*y]))
+    solve(matrix, shape)
+
+
+if __name__ == '__main__':
+    main()
