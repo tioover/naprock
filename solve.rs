c168f364984ad4e724b9a033089584fc1b67ca81
Tengfei Tsui <tioover@gmail.com>
2014-07-19 16:37:10 +0800

    Update solve.rs

--- solve.rs
+++ solve.rs
@@ -1,5 +1,8 @@
 use std::rc::Rc;
 use std::num::abs;
+use std::collections::HashMap;
+use std::collections::PriorityQueue;
+
 type N = int;
 type Parent = Option<Rc<Node>>;
 type Matrix = Vec<N>;
@@ -7,7 +10,8 @@ static X: N = 3;
 static Y: N = 3;
 
 
-enum Step {
+enum Step
+{
     Up,
     Down,
     Left,
@@ -16,8 +20,9 @@ enum Step {
 }
 
 
-struct Node {
-    matrix: Box<Matrix>,
+struct Node
+{
+    matrix: Matrix,
     shape: (N, N),
     parent: Parent,
     center: N,
@@ -26,10 +31,11 @@ struct Node {
     step: Step,
 }
 
-fn new(matrix: Box<Matrix>, shape: (N, N), center: N) -> Node {
+fn new(matrix: Box<Matrix>, shape: (N, N), center: N) -> Node
+{
     let value = valuation(matrix, shape);
     Node {
-        matrix: matrix,
+        matrix: *matrix,
         shape: shape,
         parent: None,
         center: center,
@@ -39,25 +45,24 @@ fn new(matrix: Box<Matrix>, shape: (N, N), center: N) -> Node {
     }
 }
 
-fn valuation(matrix: &Matrix, shape: (N, N)) -> f32 {
+fn valuation(matrix: &Matrix, shape: (N, N)) -> f32
+{
     let mut value = 0i;
     let (x, y) = shape;
     for i in range(0, x*y) {
         let a = matrix.get(i as uint);
         value += abs(i/y - a/y) + abs((i % y) - (a % y));
     }
-    value as f32
+    (100000 - value) as f32
 }
 
-fn spread(parent: Rc<Node>, step: Step) -> Option<Rc<Node>> {
+fn spread(parent: Rc<Node>, step: Step) -> Option<Rc<Node>>
+{
     let (x, y) = parent.shape;
     let max = x*y;
     let center = parent.center;
-    let mut new_center = center;
     let mut matrix = parent.matrix.clone();
-    let new_node = Rc::new({
-    });
-    let shift: Option<N> = match step {
+    let shift = match step {
         Up => {
             let shift = center - x;
             if shift >= 0 {Some(shift)} else {None}
@@ -87,7 +92,7 @@ fn spread(parent: Rc<Node>, step: Step) -> Option<Rc<Node>> {
                 matrix_slice[c] = matrix_slice[s];
                 matrix_slice[s] = swap;
             }
-            let value = valuation(matrix, parent.shape);
+            let value = valuation(&matrix, parent.shape);
             Some(Rc::new(Node{
                 matrix: matrix,
                 shape: parent.shape,
@@ -101,5 +106,34 @@ fn spread(parent: Rc<Node>, step: Step) -> Option<Rc<Node>> {
     }
 }
 
-fn main() {
+fn print_node(node: Rc<Node>)
+{
+    println!("matrix: {} value: {}", node.matrix.as_slice(), node.value);
+}
+
+fn solve(root: Rc<Node>) -> Rc<Node>
+{
+    let mut open = PriorityQueue::new();
+    let mut close = HashMap::new();
+    let mut solve_node = root;
+    let MAX_LOOP = 10000u;
+    open.push((root.value, box root));
+    for i in range(0, MAX_LOOP) {
+        match open.pop() { None => fail!("open empty"), Some((value, node)) => {
+            if value == 0 {return node;}
+            else if close.contains_key(&node.matrix) {continue;} // TODO: update
+            else {close.insert(node.matrix, node.copy())}
+            for step in [Up, Down, Left, Right] {
+                match spread(node, step) { None => None,
+                    Some(new_node) => open.push((new_node.value, box new_node))}
+            }}
+        }
+    }
+    solve_node
+}
+
+fn main()
+{
+    let root = Rc::new(new(box vec!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9), (X, Y), 0));
+    print_node(solve(root))
 }
